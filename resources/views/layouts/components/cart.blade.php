d6565976dbbf2133fdbb6b0907aeab0fe5b6eed2
xiaoheixi <justinzhao893@gmail.com>
2020-09-24 18:22:56 +1000

    30th commit!

--- resources/views/layouts/components/cart.blade.php
+++ resources/views/layouts/components/cart.blade.php
@@ -1,23 +1,25 @@
 <p>Your Cart</p>
 <table style="width:100%">
-  <tr>
-    <th>Name</th>
-    <th>Price</th>
-    <th>Quantity</th>
-    <th>Action</th>
-  </tr>
-  @foreach(\Cart::getContent() as $item)
+    <tr>
+        <th>Name</th>
+        <th>Price</th>
+        <th>Quantity</th>
+        <th>Action</th>
+    </tr>
+    @foreach(\Cart::getContent() as $item)
     <tr>
         <td>{{ $item->name }}</td>
         <td>
             ${{Cart::get($item->id)->getpriceSum()}}
         </td>
-      <form action ="{{route('cart.update', $item->id)}}">
-      <td><input name="quantity" type="number" value = "{{ $item->quantity }}"><input type="submit" value="update"></td>
-      </form>
-      <td><a href="{{ route('cart.destroy', $item->id)}}">Delete</a>
+        <form action="{{route('cart.update', $item->id)}}">
+            <td><input name="quantity" type="number" value="{{ $item->quantity }}"><input class ="updateCartButton" type="submit" value="update">
+            </td>
+        </form>
+        <td><a href="{{ route('cart.destroy', $item->id)}}"><button class="deleteCartButton">Delete</button></a>
     </tr>
-  @endforeach
+    @endforeach
 </table>
 <h3>Total Price : ${{\Cart::getTotal()}}</h3>
-<a name="" id="" class="btn btn-primary" href="{{ route('cart.checkout') }}" role="button">Proceed to checkout</a>
+<a name="" id="" class="btn btn-outline-warning" href="{{ route('cart.checkout') }}" role="button">Proceed to
+    checkout</a>
