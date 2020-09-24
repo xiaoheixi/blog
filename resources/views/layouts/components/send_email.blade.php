8edd64f8c4625d63f3dc6d6459ed347fa89aab8f
xiaoheixi <justinzhao893@gmail.com>
2020-09-24 19:18:04 +1000

    33rd Commit!

--- resources/views/layouts/components/send_email.blade.php
+++ resources/views/layouts/components/send_email.blade.php
@@ -1,20 +1,20 @@
 @if(count($errors) > 0)
-    <div class="alert alert-danger">
-        <button type="button" class="close" data-dismiss="alert">x</button>
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
+<div class="alert alert-danger">
+    <button type="button" class="close" data-dismiss="alert">x</button>
+    <ul>
+        @foreach($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
 @endif
 @if($message = Session::get('success'))
-    <div class="alert alert-success alert-block">
-        <button type="button" class="close" data-dismiss="alert">x</button>
-        <strong>{{ $message }}</strong>
-    </div>
+<div class="alert alert-success alert-block">
+    <button type="button" class="close" data-dismiss="alert">x</button>
+    <strong>{{ $message }}</strong>
+</div>
 @endif
-<form method="post" action="{{ url('sendemail/send') }}">
+<form class="sendEmailForm" method="post" action="{{ url('sendemail/send') }}">
     {{ csrf_field() }}
     <div class="form-group">
         <label>Enter Your Name</label>
@@ -29,5 +29,5 @@
         <textarea name="message" class="form-control"></textarea>
     </div>
     <div class="form-group">
-        <input class="btn btn-outline-warning" type="submit" name="send" value="Send" class="btn btn-info"/>
+        <input class="btn btn-outline-warning" type="submit" name="send" value="Send" class="btn btn-info" />
 </form>
