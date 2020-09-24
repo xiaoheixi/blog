d6f66eedc7181a77a8a1c5a644b0c1e1ebf19c95
xiaoheixi <justinzhao893@gmail.com>
2020-09-24 20:26:14 +1000

    32nd Commit!

--- resources/views/layouts/components/navbar.blade.php
+++ resources/views/layouts/components/navbar.blade.php
@@ -1,4 +1,8 @@
 <ul>
+    <li>
+        <a class="navBarLinks" href="/"><img style="height:1px, width:1px"
+                src="/images/Marketing_4_Growth_Logo.jpg"></a>
+    </li>
     @foreach($navContent as $nav)
     <li>
         <a class="navBarLinks" href="{!!$nav->navLink!!}">{!!$nav->navName!!}</a>
