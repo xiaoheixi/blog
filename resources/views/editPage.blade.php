3211236653481f9bb134136c02ab3ae5f66aca88
xiaoheixi <justinzhao893@gmail.com>
2020-09-27 23:40:22 +1000

    56th commit!

--- resources/views/editPage.blade.php
+++ resources/views/editPage.blade.php
@@ -19,10 +19,7 @@
     <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <script type="text/javascript">
         tinymce.init({
-            selector: '#pageContent',
-            body_class: 'my_class',
-            content_css: '/css/main.css',
-            inline: true
+            selector: '#pageContent'
         });
 
     </script>
