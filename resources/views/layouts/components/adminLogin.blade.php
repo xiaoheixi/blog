ed37cce813dcb6c213532320cf6ff6077a7f7f33
vpatel96 <patelvirenau@gmail.com>
2020-09-28 21:53:16 +1000

    69 commit

--- resources/views/layouts/components/adminLogin.blade.php
+++ resources/views/layouts/components/adminLogin.blade.php
@@ -4,7 +4,7 @@
     <input type="text" id="email" name="email"><br>
     @error('email') <p style="color: red">{{ $message }}</p> @enderror
     <label for="password">Password:</label><br>
-    <input type="text" id="password" name="password"><br><br>
+    <input type="password" id="password" name="password"><br><br>
     @error('password') <p style="color: red">{{ $message }}</p> @enderror
     <input class="btn btn-outline-warning" type="submit" value="Submit">
 </form>
