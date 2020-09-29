9d937985532f08bd91a64a39641cc85d2cce8fd1
omar441-lab <sm.chowdhury710@gmail.com>
2020-09-30 02:45:06 +1000

    72nd commit!

--- resources/views/layouts/components/shop.blade.php
+++ resources/views/layouts/components/shop.blade.php
@@ -1,13 +1,22 @@
-<div class="container-fluid">
+<div class="container">
+
     <div class="row">
         @foreach ($allProducts as $product)
-        <div class="card col-md-4">
-            <div class="card-body">
-                <img class="productImage" src="/{{ $product->cover_img}}" alt="{{ $product->cover_img }}">
-                <h4 class="card-title">{{ $product->name }}</h4>
-                <p>{{ $product->description}}</p>
-                <p class="price">${{ $product->price}}</p>
-                <a href="{{ route('cart.add', $product->id) }}" class="btn btn-outline-warning">Add to Cart</a>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-10">
+                <img src="/{{ $product->cover_img}}" alt="{{ $product->cover_img }}">
+                <div class="card-body">
+                    <h4 class="{{ $product->name }}">
+
+                    </h4>
+                    <h5>${{ $product->price}}</h5>
+                    <p class="card-text">{{ $product->description}}</p>
+
+                </div>
+                <div class="card-footer text-right">
+                    <a href="{{ route('cart.add', $product->id) }}" class="btn btn-outline-warning">Add to Cart</a>
+                </div>
             </div>
         </div>
         @endforeach
