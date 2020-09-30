1f40c0ed3e695d6c212b5f22de5633263a7ec982
omar441-lab <sm.chowdhury710@gmail.com>
2020-09-30 18:29:27 +1000

    75th

--- resources/views/layouts/components/service.blade.php
+++ resources/views/layouts/components/service.blade.php
@@ -1,14 +1,24 @@
-<div class="container-fluid">
+</div>
+<div class="container">
+
     <div class="row">
         @foreach ($serviceContent as $service)
-        <div class="cardServices h-4 col-md-4">
-            <div class="card-body">
-                <h4 class="card-title">{{ $service->serviceName }}</h4>
-                <p>{{ $service->serviceDescription}}</p>
-                <a href="{{ $service->serviceLink }}" class="btn btn-outline-warning">Learn More</a>
+
+        <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-10">
+                <div class="card-body">
+                    <h4 class=" {{ $service->serviceName }}">
+
+                    </h4>
+
+                    <p class="card-text">{{ $service->serviceDescription}}</p>
+
+                </div>
+                <div class="card-footer text-right">
+                    <a href="{{ $service->serviceLink }}" class="btn btn-outline-warning">Learn More</a>
+                </div>
             </div>
         </div>
         @endforeach
     </div>
-
 </div>
