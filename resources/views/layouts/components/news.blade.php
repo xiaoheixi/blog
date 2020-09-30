711a2b8aece0cd5bd238bf6bc8d98a84263258cd
omar441-lab <sm.chowdhury710@gmail.com>
2020-09-30 20:17:07 +1000

    76th

--- resources/views/layouts/components/news.blade.php
+++ resources/views/layouts/components/news.blade.php
@@ -1,15 +1,23 @@
-<div class="news-page page">
-    <div class="container-fluid">
-        <div class="row">
-            @foreach ($newsContent as $news)
-            <div class="cardNews col-md-12">
+<div class="container">
+
+    <div class="row justify-content-center">
+        @foreach ($newsContent as $news)
+
+        <div class="col-md-12">
+            <div class="card h-10">
                 <div class="card-body">
-                    <h4 class="card-title">{{ $news->newsName }}</h4>
-                    <p>{{ $news->newsDescription}}</p>
+                    <h4 class=" {{ $news->newsName }}">
+
+                    </h4>
+
+                    <p class="card-text">{{ $news->newsDescription}}</p>
+
+                </div>
+                <div class="card-footer text-right">
                     <a href="{{ $news->newsLink }}" class="btn btn-outline-warning">Learn More</a>
                 </div>
             </div>
-            @endforeach
         </div>
+        @endforeach
     </div>
 </div>
